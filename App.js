b90d0ecaf0b181a6903b2f072f88c31e17f419d1
subhash30897 <40037307+subhash30897@users.noreply.github.com>
2024-01-07 20:00:07 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,31 @@
+/**
+ *
+ *
+ *  <div id="parent">
+ *      <div id="child">
+ *          <h1>I am h1</h1>
+ *      </div>
+ *  </div>
+ *
+ * React Element(object) => html(Browser understands);
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+]);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React!"
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
